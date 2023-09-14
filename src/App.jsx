99bb1f37732c51b345239b5c015aa40a09d1174e
eftekher-alam <eftekher@yahoo.com>
2023-09-14 18:25:30 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-5xl font-extrabold text-center'>Tailwind is installed</h1>
+    </>
+  )
+}
+
+export default App
